dbb920b7fe353cb8271399782441659cc456d9b9
Tim Jones <tim@timjones.tw>
2012-10-14 11:07:02 +0800

    Added Meshellator.nuspec

--- src/Meshellator/Properties/AssemblyInfo.cs
+++ src/Meshellator/Properties/AssemblyInfo.cs
@@ -5,8 +5,18 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Meshellator")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("3D asset import library for .NET 4.0 and Silverlight 4. Supported formats are Autodesk 3DS and Lightwave OBJ.")]
 [assembly: AssemblyCulture("")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("1ab5840e-5e00-4708-b6b8-1628a903a1e0")]
+
+[assembly: AssemblyConfiguration("Retail")]
+[assembly: AssemblyCompany("Tim Jones")]
+[assembly: AssemblyProduct("Meshellator")]
+[assembly: AssemblyCopyright("Copyright ©2012 Tim Jones")]
+[assembly: AssemblyTrademark("timjones.tw")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
